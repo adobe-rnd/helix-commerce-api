856fa4e0906b612ffd3b30061459af7078f57889
Max Edell <maxed@adobe.com>
2025-03-04 15:15:22 -0800

    more logs

--- src/routes/content/media.js
+++ src/routes/content/media.js
@@ -19,11 +19,13 @@ export default async function handler(ctx) {
   const {
     env,
     url,
+    log,
     config: { org, site },
   } = ctx;
 
   const filename = url.pathname.split('/').pop();
   const key = `${org}/${site}/media/${filename}`;
+  log.debug('fetching media: ', key);
   const resp = await env.CATALOG_BUCKET.get(key);
   if (!resp) {
     return errorResponse(404, 'File not found');
