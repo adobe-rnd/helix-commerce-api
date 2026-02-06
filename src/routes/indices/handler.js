54236bf4399e81447ca69ab6e49358107f92d584
Max Edell <maxed@adobe.com>
2026-02-06 11:50:25 -0800

    fix: list indices api

--- src/routes/indices/handler.js
+++ src/routes/indices/handler.js
@@ -162,6 +162,30 @@ async function remove(ctx) {
 }
 
 /**
+ * Get the list of created indices for a site
+ * Reponds with the registry entries
+ *
+ * @param {Context} ctx
+ * @returns {Promise<Response>}
+ */
+async function list(ctx) {
+  const { org, site } = ctx.requestInfo;
+  const storage = StorageClient.fromContext(ctx);
+  const { data: registry } = await storage.fetchIndexRegistry(org, site);
+  return new Response(JSON.stringify({
+    indices: Object.entries(registry).map(([path, entry]) => ({
+      path,
+      lastModified: new Date(entry.lastmod).toISOString(),
+    })),
+  }), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
+
+/**
  * @type {RouteHandler}
  */
 export default async function handler(ctx) {
@@ -169,6 +193,18 @@ export default async function handler(ctx) {
   const {
     path, method, org, site,
   } = requestInfo;
+
+  console.log('indices handler', method, path);
+
+  if (method === 'OPTIONS') {
+    return optionsHandler(['GET', 'POST', 'DELETE'])(ctx);
+  }
+
+  if (method === 'GET') {
+    ctx.authInfo.assertPermissions('index:read');
+    ctx.authInfo.assertOrgSite(org, site);
+    return list(ctx);
+  }
 
   if (!path) {
     return errorResponse(404, 'path is required');
@@ -183,8 +219,6 @@ export default async function handler(ctx) {
       ctx.authInfo.assertPermissions('index:write');
       ctx.authInfo.assertOrgSite(org, site);
       return remove(ctx);
-    case 'OPTIONS':
-      return optionsHandler(['POST', 'DELETE'])(ctx);
     default:
       return errorResponse(405, 'method not allowed');
   }
